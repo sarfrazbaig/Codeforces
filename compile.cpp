fd6dd278a609d4c21233df5c40a09524fb8571eb
Sarfraz baig <sarfrazbaig55@gmail.com>
2017-06-17 21:11:07 +0530

    Initial Commit

--- compile.cpp
+++ compile.cpp
@@ -1,69 +1,63 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+long long int summer(vector<long long int>a)
+{
+	long long int sum = 0;
+	for(int i=0;i<a.size();i++)
+	{
+		sum += a[i];
+	}
+
+	return sum;
+}
+
+
+
 int main()
 {
 	long long int n;
 	cin>>n;
-	int round = 1;
+	long long previous_sum = 0;
+	
 	int tests = 3;
-	map<long long int,long long int>a;
-	map<long long int,bool>repeated;
-	vector<long long int> ans;
+	int round = 1;
+	vector<long long int>ans;
 	while(tests--)
 	{
-
-		long long int numbers = n--;
-		long long int temp;
+		vector<long long int>a;
+		long long current_sum = 0;
+		int numbers = n--;
 		for(int i=0;i<numbers;i++)
 		{
+			long long int temp;
 			cin>>temp;
-			
-			a[temp]++;
-			if(round==1)
-			{if(a[temp]>1)
-			{
-				repeated[temp]=true;	
-			}
-			else
-			{
-				repeated[temp]=false;
-			}
+			a.push_back(temp);
 		}
 
-		}
-
-
-		// map<long long int,long long int>::iterator it;
-		// for(it=a.begin();it!=a.end();it++)
-		// {
-		// 	cout<<it->first<<" "<<it->second<<endl;
-		// }
-
+		current_sum = summer(a);
+		// cout<<"Current sum"<<current_sum<<endl;
+		// cout<<"Previous sum"<<previous_sum<<endl; 	
 		if(round!=1)
 		{
-			long long int previous=-1;
-			map<long long int,long long int>::iterator it;
-			for(it=a.begin();it!=a.end();++it)
-			{
-				if(it->second!=round && it->first!=previous && repeated[it->first]==false)
-				{
-					ans.push_back(it->first);
-					previous = it->first;
-					break;
-				}
-				else if(it->second==previous && repeated[it->first]==true)
-				{
-					ans.push_back(it->first);
-					previous = it->first;
-				}
-			}
+			ans.push_back(previous_sum-current_sum);
+			previous_sum = current_sum;
+			current_sum = 0;
+		}
+		else
+		{
+			
+			previous_sum = current_sum;
+			current_sum = 0;
 		}
 		round++;
 
-	}	
+	}
+
 	for(int i=0;i<ans.size();i++)
 	{
 		cout<<ans[i]<<endl;
 	}
+	
+
 }
